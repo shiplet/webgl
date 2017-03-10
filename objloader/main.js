85e449180ea066f8777ba09aa213937fdad8b605
Shiplet <mnshiplet@gmail.com>
2017-03-09 17:53:30 -0700

    r2

--- objloader/main.js
+++ objloader/main.js
@@ -1,84 +1,77 @@
 (function(THREE){
 
 
-var Item = new ObjTest({ mtlPath: 'materials/', mtl: 'BasicSpeaker.mtl', objPath: './', obj: 'BasicSpeaker.obj' })
+    var Item = new ObjTest({ mtlPath: 'obj/speaker/', mtl: 'speaker.mtl', objPath: 'obj/speaker/', obj: 'speaker.obj' })
 
 
-function ObjTest(config) {
-    this.config = config
-    this.container = null
-    this.camera = null
-    this.scene = null
-    this.renderer = null
-    this.mouseX = 0
-    this.mouseY = 0
-    this.windowHalfX = window.innerWidth / 2.0
-    this.windowHalfY = window.innerHeight / 2.0
-    this.clearColor = 0x000000
+    function ObjTest(config) {
+        this.config = config
+        this.container = null
+        this.camera = null
+        this.scene = null
+        this.renderer = null
+        this.mouseX = 0
+        this.mouseY = 0
+        this.windowHalfX = window.innerWidth / 2.0
+        this.windowHalfY = window.innerHeight / 2.0
+        this.clearColor = 0x000000
+        this.mesh = null
+        this.shouldAutoRotate = false
+        this.diffuseTexture = null
+        this.bumpTexture = null
+        this.emissiveTexture = null
+        this.specularTexture = null
 
-    this.init = function() {
-        this.container = document.createElement( 'div' )
-        document.body.appendChild( this.container )
-        this.camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 2000 )
-        this.camera.position.x = 8
-        this.camera.position.y = 2
-        this.camera.position.z = 8
+        this.init = function() {
+            this.container = document.createElement( 'div' )
+            document.body.appendChild( this.container )
+            this.camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 2000 )
+            this.camera.position.x = 0
+            this.camera.position.y = 10
+            this.camera.position.z = 20
 
-        this.scene = new THREE.Scene()
+            this.scene = new THREE.Scene()
 
-        var ambient = new THREE.AmbientLight( 0x444444 )
-        this.scene.add(ambient)
+            var ambient = new THREE.AmbientLight( 0xffffff )
+            this.scene.add(ambient)
 
-        var directionalLight = new THREE.DirectionalLight( 0xffeedd )
-        directionalLight.position.set( 0, 0, 1 ).normalize()
-        this.scene.add( directionalLight )
-
-        var onProgress = function( xhr ) {
-            if( xhr.lengthComputable ) {
-                var percentComplete = xhr.loaded / xhr.total * 100
-                console.log( Math.round(percentComplete, 2) + '% downloaded' )
+            for(var i = 0; i < 3; i++) {
+                var directionalLight = new THREE.DirectionalLight( 0xffeedd )
+                directionalLight.position.set( Math.random() * 10, Math.random() * 10, Math.random() * 10 ).normalize()
+                this.scene.add( directionalLight )
             }
-        }.bind(this)
 
-        var onError = function( xhr ) {}.bind(this)
+            var hemisphereLight = new THREE.HemisphereLight( 0xffffff )
+            this.scene.add(hemisphereLight)
 
-        var mtlLoader = new THREE.MTLLoader()
-        console.log(this.config.mtlPath)
-        mtlLoader.setPath( this.config.mtlPath )
-        mtlLoader.load( this.config.mtl, function( materials ){
-            if(materials.materialsArray > 0)
-            {
-                materials.preload()
+            var loader = new THREE.JSONLoader()
+            loader.load('obj/r2d2/r2.js', function(geometry, materials){
+                this.mesh = new THREE.Mesh( geometry, new THREE.MultiMaterial( materials ) )
+                this.scene.add(this.mesh)
+            }.bind(this))
 
-                var objLoader = new THREE.OBJLoader()
-                objLoader.setMaterials( materials )
-                objLoader.setPath( this.config.objPath )
-                objLoader.load( this.config.obj, function( object ){
-                    object.position.y = 0
-                    this.scene.add( object )
-                }.bind(this), onProgress, onError )
-            }
-            else
-            {
-                var objLoader = new THREE.OBJLoader()
-                objLoader.setPath( this.config.objPath )
-                objLoader.load( this.config.obj, function(object){
-                    object.position.y = 0
-                    this.scene.add( object )
-                }.bind(this) )
-            }
-        }.bind(this) )
+            this.renderer = new THREE.WebGLRenderer({alpha: true})
+            this.renderer.setPixelRatio( window.devicePixelRatio )
+            this.renderer.setSize( window.innerWidth, window.innerHeight )
+            this.renderer.setClearColor( this.clearColor, 0 )
+            this.container.appendChild( this.renderer.domElement )
 
-        this.renderer = new THREE.WebGLRenderer()
-        this.renderer.setPixelRatio( window.devicePixelRatio )
-        this.renderer.setSize( window.innerWidth, window.innerHeight )
-        this.renderer.setClearColor( this.clearColor, 1 )
-        this.container.appendChild( this.renderer.domElement )
+            this.controls = new THREE.OrbitControls( this.camera, this.renderer.domElement )
 
-        this.controls = new THREE.OrbitControls( this.camera, this.renderer.domElement )
-
-        window.addEventListener( 'resize', this.onWindowResize, false )
+            window.addEventListener( 'resize', this.onWindowResize, false )
+            window.addEventListener( 'mousedown', this.onWindowMouseDown, false )
+            window.addEventListener( 'mouseup', this.onWindowMouseUp, false )
     }.bind(this)
+
+    this.onProgress = function( xhr ){
+        if( xhr.lengthComputable )
+        {
+            var percentComplete = xhr.loaded / xhr.total * 100
+            console.log( Math.round( percentComplete, 2 ) +  "% downloaded" )
+        }
+    }.bind(this)
+
+    this.onError = function(){}.bind(this)
 
     this.onWindowResize = function() {
         this.windowHalfX = window.innerWidth / 2.0
@@ -90,12 +83,26 @@ function ObjTest(config) {
         this.renderer.setSize( window.innerWidth, window.innerHeight )
     }.bind(this)
 
+    this.onWindowMouseDown = function() {
+        this.shouldAutoRotate = false
+    }.bind(this)
+
+    this.onWindowMouseUp = function() {
+        this.shouldAutoRotate = true
+    }.bind(this)
+
     this.animate = function() {
         requestAnimationFrame( this.animate )
         this.render()
     }.bind(this)
 
     this.render = function() {
+        if(this.mesh && this.mesh.rotation && this.shouldAutoRotate)
+        {
+            // this.mesh.rotation.x -= 0.005
+            this.mesh.rotation.y += 0.005
+            // this.mesh.rotation.z += 0.005
+        }
         this.camera.lookAt( this.scene.position )
         this.renderer.render( this.scene, this.camera )
     }.bind(this)
